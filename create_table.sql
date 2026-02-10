a1a582389b9ce0f77490b565869fd550bb66d8fa
Shivansh <shivanshpatra.patra@gmail.com>
2026-02-10 23:39:50 +0530

    Create create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Inventory (
+    Date DATE,
+    ProductID VARCHAR(50),
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    CurrentStock INT,
+    SalesQuantity INT,
+    UnitCost DECIMAL(10,2),
+    SellingPrice DECIMAL(10,2),
+    LeadTimeDays INT
+);
